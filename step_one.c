e19cdbe50c8b21520ea35f19167b7e4444ab8041
120864 <michael.schreiber@sncorp.com>
2025-02-13 14:38:10 -0800

    more practice

--- step_one.c
+++ step_one.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+#include <unistd.h> 
 
 int first_method();
 void second_variables_intro();
 void practicing_bools();
+void format_specifiers();
+int arithmetic_operators();
+void user_input();
+void self_destruct_sequence();
 
 int main(){
     
+   
     //first_method();
     //second_variables_intro();
     //practicing_bools();
 
-    format_specifiers();
+    //format_specifiers();
+    // arithmetic_operators();
+    //user_input_addressOfOperator();
+    //if_statement_practice();
+
+    char grade = 'a';
+    switch_case_practice(grade);
+
 
     return 0; //return 0 if program runs successfully, else return 1
 }
@@ -66,3 +80,128 @@ void format_specifiers(){
     printf("ITEM1: $%100.2f \n", item1);
     printf("ITEM2: $%8.2f \n", item2);
     printf("ITEM3: $%8.2f \n", item3);
+
+}
+
+int arithmetic_operators(){
+    int x = 5;
+    int y = 2;
+
+    float z = x/(float)y;
+
+    printf("%.2f", z);
+
+    return 0;
+}
+
+void user_input_addressOfOperator(){
+    char name[25];
+    int age;
+
+    // scanf ONLY reads up until any whitespace, so if a user types in "First Name" the program will only read "First"
+    // fgets is the function to solve that problem, it requires (nameOfVariable, length, stdin). the stdin call means "standard input" 
+
+    printf("How Old are you: ");
+    scanf("%d", &age); // the & is the "address of" operator. this says "get user input and assign it to the age variable"
+    printf("\nWhat is your name: ");
+    fgets(name, 25, stdin); 
+    //scanf("%s", &name);
+
+    printf("\nHello %s, how are you?", name);
+    printf("\nyou are %d years old", age);
+
+    return 0;
+}
+
+void if_statement_practice(){
+    int age;
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("\n You are %d years old,", age);
+
+    if (age < 18){
+        printf("\n You are a minor");
+    }
+
+    else if (age < 0) {
+        printf("YOU HAVENT BEEN BORN YET LOL");
+    }
+
+    else {
+        printf("\n You are an adult.");
+    }
+
+
+
+
+
+
+
+}
+
+
+void switch_case_practice(char grade){
+   const char* statement;
+   bool destruct;
+
+    switch(grade){
+        case 'A' || 'a':
+        statement = "Youre a rockstar!";
+        break;
+
+        case 'B':
+        statement = "try harder";
+        break;
+
+        case 'C':
+        statement = "better luck next year";
+        break;
+
+        default:
+        statement = "invalid grade";
+        destruct = true;
+        break;
+
+    }
+
+    //printf("INVALID GRADE (%s), INITIATE SELF DESTRUCT", grade);
+
+    if (destruct == true){
+
+        
+
+        for (int i = 1; i <= 100; ++i) {
+            self_destruct_sequence(i);
+            usleep(50000); // Sleep for 50 milliseconds to simulate progress
+        }
+        
+        printf("\n");
+        return;
+    }
+
+    printf(statement);
+}
+
+
+#define BAR_WIDTH 50
+
+void self_destruct_sequence(int percentage)
+    {
+        int filled_length = (percentage * BAR_WIDTH) / 100;
+        int empty_length = BAR_WIDTH - filled_length;
+
+        printf("\rSELF DESTRUCT SEQUENCE: [");
+
+        for (int i = 0; i < filled_length; ++i) {
+            printf("#");
+        }
+        for (int i = 0; i < empty_length; ++i) {
+            printf(" ");
+        }
+
+        printf("] %d%%", percentage);
+        fflush(stdout);
+
+    }
